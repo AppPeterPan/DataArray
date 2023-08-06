ee9c917d4f3a584521e41c39227ef89a00b903a0
Peter Pan <apppeterpan@gmail.com>
2023-08-06 22:21:48 +0800

    Create Outfielder.swift

---
+++ Outfielder.swift
@@ -0,0 +1,17 @@
+// Define the Outfielder struct
+struct Outfielder {
+    var name: String
+    var imgURL: String
+    var position: String
+    var number: Int
+}
+
+// Create an array of Outfielder
+var outfielders: [Outfielder] = [
+    Outfielder(name: "唐肇廷", imgURL: "/files/atts/0L087782123257884739/6唐肇廷.jpg", position: "中外野手", number: 6),
+    Outfielder(name: "邱智呈", imgURL: "/files/atts/0L088853811359500564/14邱智呈.jpg", position: "右外野手", number: 14),
+    Outfielder(name: "陳傑憲", imgURL: "/files/atts/0L087782027721249240/24陳傑憲.jpg", position: "中外野手", number: 24),
+    Outfielder(name: "蘇智傑", imgURL: "/files/atts/0L087782029558280710/32蘇智傑.jpg", position: "左外野手", number: 32),
+    Outfielder(name: "林安可", imgURL: "/files/atts/0L087782038188845240/77林安可.jpg", position: "右外野手", number: 77)
+]
+
